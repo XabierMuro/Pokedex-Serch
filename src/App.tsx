a9379dd0ad09ecccae43413054510c5f7204a87e
Murillo-EL-Navajas <xabiermuro1612@gmail.com>
2024-05-31 18:06:31 +0200

    pokemon card starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import styles from './index.module.css'
+
 function App() {
-  return <main>My Pokédex</main>
+  return (
+    <main className={styles.pokemon_card}>
+      <header className={styles.card_header}>
+        <h3>Bulbasur</h3>
+        <h3>#001</h3>
+      </header>
+      <body className={styles.pokemon_stats_container}>
+        <article className="pokemon_types">
+          <div className="pokemon_type_grass"></div>
+          <div className="pokemon_type_poison"></div>
+        </article>
+      </body>
+    </main>
+  )
 }
 
 export default App
