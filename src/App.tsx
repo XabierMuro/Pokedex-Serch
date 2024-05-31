cda42e9f0615552f2e3982f6caebdcc6780c5f18
Murillo-EL-Navajas <xabiermuro1612@gmail.com>
2024-05-31 19:32:48 +0200

    medidas del pokemon y lista de ataques

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,24 @@ function App() {
         <h3>#001</h3>
       </header>
       <body className={styles.pokemon_stats_container}>
+        {/* TODO nose si esto son articles */}
         <article className={styles.pokemon_types}>
           <div className={styles.pokemon_type_grass}>Grass</div>
           <div className={styles.pokemon_type_poison}>Poison</div>
+        </article>
+        <article className={styles.pokemon_measures}>
+          <div className={styles.pokemon_weight}>6.9 kg</div>
+          <div className={styles.pokemon_length}>0.7 m</div>
+        </article>
+        <article>
+          <ul>
+            <li>HP</li>
+            <li>ATK</li>
+            <li>DEF</li>
+            <li>SAT</li>
+            <li>SDF</li>
+            <li>SPD</li>
+          </ul>
         </article>
       </body>
     </main>
