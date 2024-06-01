4b39758a8f01a884c7a8ec5129bdc8418ac0cf16
Murillo-EL-Navajas <xabiermuro1612@gmail.com>
2024-06-01 10:50:30 +0200

    pokemon progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,13 @@ function App() {
         <h3>Bulbasur</h3>
         <h3>#001</h3>
       </header>
-      
 
       <section className={styles.pokemon_stats_container}>
-      <img
-        className={styles.pokemon_img}
-        src="../public/pokemon3.png"
-        alt="pokemon image"
-      />
+        <img
+          className={styles.pokemon_img}
+          src="../public/pokemon3.png"
+          alt="pokemon image"
+        />
         {/* TODO nose si esto son articles */}
         <article className={styles.pokemon_types}>
           <div className={styles.pokemon_type_grass}>Grass</div>
@@ -25,13 +24,31 @@ function App() {
           <div className={styles.pokemon_length}>0.7 m</div>
         </article>
         <article>
-          <ul>
-            <li>HP</li>
-            <li>ATK</li>
-            <li>DEF</li>
-            <li>SAT</li>
-            <li>SDF</li>
-            <li>SPD</li>
+          <ul className={styles.pokemon_stats_list}>
+            <li className={styles.pokemon_stats}>
+              HP<span className={styles.pokemon_stat_number}>045</span>
+              <progress className={styles.pokemon_progress_bar} value={0.6} />
+            </li>
+            <li className={styles.pokemon_stats}>
+              ATK<span className={styles.pokemon_stat_number}>045</span>
+              <progress className={styles.pokemon_progress_bar} value={0.3} />
+            </li>
+            <li className={styles.pokemon_stats}>
+              DEF<span className={styles.pokemon_stat_number}>045</span>
+              <progress className={styles.pokemon_progress_bar} value={0.3} />
+            </li>
+            <li className={styles.pokemon_stats}>
+              SAT<span className={styles.pokemon_stat_number}>045</span>
+              <progress className={styles.pokemon_progress_bar} value={0.9} />
+            </li>
+            <li className={styles.pokemon_stats}>
+              SDF<span className={styles.pokemon_stat_number}>045</span>
+              <progress className={styles.pokemon_progress_bar} value={0.5} />
+            </li>
+            <li className={styles.pokemon_stats}>
+              SPD<span className={styles.pokemon_stat_number}>045</span>
+              <progress className={styles.pokemon_progress_bar} value={0.5} />
+            </li>
           </ul>
         </article>
       </section>
