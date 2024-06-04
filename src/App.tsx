f185efbbbf82fbdddce0c6f958552c3b2b02dc8c
Murillo-EL-Navajas <xabiermuro1612@gmail.com>
2024-06-04 18:22:30 +0200

    header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './index.css'
 import Card from'./components/card/card'
+import Header from './components/header/header'
 function App() {
   return (
     <>
+
+    <Header></Header>
+    <div className='container'>
     <Card></Card>
+    </div>
     </>
   )
 }
