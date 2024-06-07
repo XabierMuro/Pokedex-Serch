aad88460954bfabe65982f700a05b9a84e7983d9
Izar Ripoll <ripoll.146287@e.unavarra.es>
2024-06-07 19:37:02 +0200

    change css for input active implemented

--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -1,19 +1,25 @@
 import styles from './searchBar.module.css'
 import SearchIcon from '../../images/searchIcon.svg'
+import { useState } from 'react'
 
 export function SearchBar() {
+  const [isFocused, setIsFocused] = useState(false)
   return (
     <>
       <div className={styles.search_container}>
         <img
           src={SearchIcon}
           alt="Search Icon"
-          className={styles.search_icon}
+          className={isFocused ? styles.search_icon_active : styles.search_icon}
         />
         <input
-          className={styles.search_input}
+          className={
+            isFocused ? styles.search_input_active : styles.search_input
+          }
           placeholder="Search a Pokemon..."
           type="text"
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
         />
       </div>
     </>
