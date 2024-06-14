4a390dceb57cafef68224a65b6b558c763a90910
Izar Ripoll <ripoll.146287@e.unavarra.es>
2024-06-14 18:34:51 +0200

    Loading cards and notFound message implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Card } from './components/card/card'
 import { Header } from './components/header/header'
 import { SearchBar } from './components/searchBar/searchBar'
 import { Footer } from './components/footer/footer'
+import { NotFound } from './components/notFound/notFound'
 import { LoadingCard } from './components/loadingCard/loadingCard'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
@@ -78,12 +79,23 @@ type Pokemon = {
 function App() {
   const [pokemons, setPokemons] = useState<PokemonData[]>([])
   const [filteredPokemons, setFilteredPokemons] = useState<PokemonData[]>([])
+  const [search, setSearch] = useState<String>('')
+
+  useEffect(() => {
+    fetchPokemon()
+  }, [])
 
   const handleChange = (query: string) => {
     setFilteredPokemons(
       pokemons.filter(pokemon => pokemon.name.includes(query)),
     )
+    setSearch(query)
   }
+  const emptyArray = []
+  for (let i = 0; i < 151; i++) {
+    emptyArray.push(i)
+  }
+
   const url = `https://pokeapi.co/api/v2/pokemon?limit=151`
 
   const fetchPokemon = async () => {
@@ -95,22 +107,22 @@ function App() {
         const response = await axios.get(pokemon.url)
         const data: PokemonDTO = response.data
         const types = data.types.map(type => type.type.name)
-        const stats = data.stats.map(stats => stats.base_stat)
-        const stats2: Stats = {
-          hp: stats[0],
-          atk: stats[1],
-          def: stats[2],
-          sat: stats[3],
-          sdf: stats[4],
-          spd: stats[5],
+        const base_stats = data.stats.map(stats => stats.base_stat)
+        const stats: Stats = {
+          hp: base_stats[0],
+          atk: base_stats[1],
+          def: base_stats[2],
+          sat: base_stats[3],
+          sdf: base_stats[4],
+          spd: base_stats[5],
         }
         return {
           name: data.name,
           id: data.id,
-          weight: data.weight,
-          height: data.height,
+          weight: data.weight / 10,
+          height: data.height / 10,
           sprites: data.sprites.front_default,
-          stats: stats2,
+          stats: stats,
           types: types,
         }
       })
@@ -124,69 +136,43 @@ function App() {
     }
   }
 
-  /*const fetchPokemon = async () => {
-    try {
-      let url = 'https://pokeapi.co/api/v2/pokemon?limit=20'
-      const allPokemons = []
-
-      while (url) {
-        const req = await axios.get(url)
-        const pokemones = req.data.results
-
-        const promises = pokemones.map((pokemon: Pokemon) =>
-          axios
-            .get(pokemon.url)
-            .then(response => {
-              const data = response.data
-              console.log(data)
-              const types = data.types.map((type: any) => type.type.name)
-              const stats = data.stats.map((stats: any) => stats.base_stat)
-
-              return {
-                name: data.name,
-                id: data.id,
-                weight: data.weight,
-                height: data.height,
-                sprites: data.sprites.front_default,
-                stats: stats,
-                types: types,
-              }
-            })
-            .catch(error => {
-              console.error('Error al obtener los datos del pokemon:', error)
-              return null
-            }),
-        )
-
-        const results = await Promise.all(promises)
-        const validResults = results.filter(result => result !== null)
-        allPokemons.push(...validResults)
-
-        url = req.data.next
-      }
-
-      setPokemons(allPokemons)
-    } catch (error) {
-      console.error('Error al obtener los datos de los pokemones:', error)
-    }
-  }*/
-
-  useEffect(() => {
-    fetchPokemon()
-  }, [])
-  return (
-    <>
+  if (pokemons.length === 0) {
+    return (
       <div className="container">
-        <Header></Header>
-        <SearchBar onChange={handleChange}></SearchBar>
+        <Header />
+        <SearchBar onChange={handleChange} />
         <div className="container_pokemon">
-          {filteredPokemons.map((pokemon, key) => (
-            <Card key={key} pokemon={pokemon} />
+          {emptyArray.map(() => (
+            <LoadingCard />
           ))}
         </div>
-        <Footer></Footer>
+        <Footer />
       </div>
-    </>
+    )
+  }
+
+  if (filteredPokemons.length === 0 && pokemons.length !== 0) {
+    return (
+      <div className="container">
+        <Header />
+        <SearchBar onChange={handleChange} />
+        <NotFound notFoundMessage={search} />
+        <Footer />
+      </div>
+    )
+  }
+
+  return (
+    <div className="container">
+      <Header />
+      <SearchBar onChange={handleChange} />
+      <div className="container_pokemon">
+        {filteredPokemons.map((pokemon, key) => (
+          <Card key={key} pokemon={pokemon} />
+        ))}
+      </div>
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/components/notFound/notFound.tsx
@@ -0,0 +1,17 @@
+import styles from './notFound.module.css'
+import notFoundIcon from '../../images/notFoundIcon.svg'
+
+export function NotFound({ notFoundMessage }: { notFoundMessage: string }) {
+  return (
+    <div className={styles.container}>
+      <img
+        className={styles.notFound_image}
+        src={notFoundIcon}
+        alt={`Not found icon`}
+      />
+      <p className={styles.notFound_message}>
+        There is no results for {notFoundMessage}
+      </p>
+    </div>
+  )
+}
